0710be82ad4b7a5278d31fbf287f1456cf113793
Mario-Alberto-Gonzalez <113622834+Mario-Alberto-Gonzalez@users.noreply.github.com>
2022-12-09 08:24:03 -0500

    Cambio endpoints

--- src/main/java/com/example/ApiG11/Controladores/UsuarioControlador.java
+++ src/main/java/com/example/ApiG11/Controladores/UsuarioControlador.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,26 +16,27 @@ import com.example.ApiG11.Modelos.UsuarioModelo;
 import com.example.ApiG11.Servicios.UsuarioServicio;
 
 @RestController
-//http://localhost:8080/usuario
+//https://paginadeportesbackend-production.up.railway.app/usuario
 @RequestMapping("/usuario")
+@CrossOrigin("*")
 public class UsuarioControlador {
 
     @Autowired
     UsuarioServicio servicio;
 
-    //http://localhost:8080/usuario/guardar
+    //https://paginadeportesbackend-production.up.railway.app/usuario/guardar
     @PostMapping("/guardar")
     public UsuarioModelo guardar(@RequestBody UsuarioModelo usuario){
         return servicio.guardarUsuario(usuario);
     }
 
-    //http://localhost:8080/usuario/consultar
+    //https://paginadeportesbackend-production.up.railway.app/usuario/consultar
     @GetMapping("/consultar")
     public ArrayList<UsuarioModelo> consultarUsuario(){
         return servicio.consultarUsuarios();
     }
 
-    //http://localhost:8080/usuario/consultaFecha/
+    //https://paginadeportesbackend-production.up.railway.app/loc/usuario/consultaFecha/
     @GetMapping("/consultaFecha/{fecha}")
     public Optional<UsuarioModelo> consultaFecha(@PathVariable("fecha")Long fecha){
         return servicio.consultaFecha(fecha);
